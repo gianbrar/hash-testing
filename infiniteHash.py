2679101c5b232f34498d5b5919a5256ecc7c9fdb
mmmmmk, here's hiddenmonkey <39420031+thehiddenmonkey@users.noreply.github.com>
2019-05-31 17:14:35 -0400

    Removed feature from Infinite Hash
    I accidentally included word of a future feature, Multi Hash, in the form of an input that currently serves no purpose.

--- infiniteHash.py
+++ infiniteHash.py
@@ -2,9 +2,7 @@ x = .5
 print("Welcome to Infinite Hash.")
 hashStr = input("Please insert what you wish to be hashed.")
 y = hashStr
-hashQ = input("Do you wish to have an infinite hash [IH] or a multihash [MH]?").lower()
-if hashQ == "ih":
-    while True:
-        hashStr2 = str(hash(y))
-        print("Your hash is: " + hashStr2)
-        y = hashStr2
+while True:
+   hashStr2 = str(hash(y))
+   print("Your hash is: " + hashStr2)
+   y = hashStr2
